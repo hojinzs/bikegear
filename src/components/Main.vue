f1e77ced600066c4a5e4b98ec5c57b7e0258b284
Steve Lee <hojinzs@gmail.com>
2019-10-21 23:34:11 +0900

    gear ration calculate method

---
+++ src/components/Main.vue
@@ -0,0 +1,94 @@
+<template>
+    <div id="main">
+        <h1>Bike Gear Ratio Calculator</h1>
+        <hr>
+        <div id="input">
+            <h2>#1 DATA INPUT</h2>
+            <div id="wheel_type">
+                <h3>1. Wheel Type</h3>
+                <input name="wheel" placeholder="wheel size">
+                <input name="tire" placeholder="tire size">
+            </div>
+            <div id="Crank">
+                <h3>2. Crank</h3>
+                <div
+                v-for="(chainring, index) in crank"
+                v-bind:key="index">
+                    <input v-model.number="crank[index]" name="chainring" placeholder="chainring">
+                </div>
+            </div>
+            <div id="wheel_type">
+                <h3>3. Sporocket</h3>
+                <div
+                v-for="(cog, index) in sporocket"
+                v-bind:key="index">
+                    <input v-model.number="sporocket[index]" name="cog" placeholder="cog">
+                </div>
+            </div>
+        </div>
+        <div>
+            <h2>#2 Calculation</h2>
+
+            <h3>1. Gear Ratio</h3>
+
+            <table>
+                <tr>
+                    <td>
+                        <th>
+                        Crank / Sporocket
+                        </th>
+                    </td>
+                    <td v-for="(cog, index) in sporocket"
+                        v-bind:key="index">
+                        <th> {{ cog }} </th>
+                    </td>
+                </tr>
+                <tr v-for="(chainring, index) in crank"
+                v-bind:key="index">
+                    <td>
+                        {{ chainring }}
+                    </td>
+                    <td v-for="(cog, index) in sporocket"
+                        v-bind:key="index">
+                        {{ calRatio(chainring,cog) }}
+                    </td>
+                </tr>
+            </table>
+
+            <h3>2. Speed</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        calRatio(chainring, cog){
+            var ratio = chainring / cog
+            return ratio.toFixed(2);
+        },
+    },
+    data: function(){
+        return {
+            crank: [50,34],
+            sporocket: [11,12,13,14,15,17,19,21,24,27,30],
+            crank_preset: [
+                {
+                    name: 'mid-compact chainring',
+                    chainring: [50,34],
+                },
+            ],
+            sporoket_preset: [
+                {
+                    name: 'CR-R8000 Ultegra (11-30T)',
+                    cog: [11,12,13,14,15,17,19,21,24,27,30],
+                }
+            ]
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
