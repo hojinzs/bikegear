5cb49c51649f59ae9ef3440dc5b121fae4863a6a
unknown <stevelee@webzen.com>
2019-10-30 15:43:08 +0900

    speedbar dom add

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,9 +1,28 @@
 <template>
     <div id="Chart">
         <div
+            class="chart-wrapper"
             v-for="(gear,index) in Gears"
             :key="index">
+
             <b> #{{ index + 1}} :: {{gear.name }}</b>
+            <div 
+            class="chart-line"
+            v-for="(sprockets,index) in gear.SpeedTable.Cranks"
+            :key="index">
+
+                <span class="line-index"> {{ index }} </span>
+
+                <div
+                class="chart_bar"
+                v-for="(speeds,index) in sprockets"
+                :key="index"
+                :min_speed="speeds.minSpeed.toFixed(2)"
+                :max_speed="speeds.maxSpeed.toFixed(2)">
+                    <b>{{ index }}t</b>
+                </div>
+
+            </div>
         </div>
         max-speed :: {{ maxSpeed.toFixed(2) }} Km/h<br>
         min-speed :: {{ minSpeed.toFixed(2) }} Km/h<br>
@@ -48,6 +67,27 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped>
+<style scoped>
+.chart-wrapper{
+    width: 100%;
+    height: 200px;
+    position: relative;
+}
 
+.chart-line{
+    height: 80px;
+    height: 80px;
+    position: relative;
+}
+
+.line-index{
+    position: absolute;
+    left: 10px;
+}
+
+.chart_bar{
+    border: 1px solid black;
+    display: inline;
+    position: absolute;
+}
 </style>
