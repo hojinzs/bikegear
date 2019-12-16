f109ff290ab742ade075541a41d39c2c66fe0c12
unknown <stevelee@webzen.com>
2019-12-16 21:43:02 +0900

    차트 그리기 데모 업데이트

--- public/chart-demo.js
+++ public/chart-demo.js
@@ -14,12 +14,15 @@ class SpeedChart{
         if(_prop.width == undefined) _prop.width = this.canvas.offsetWidth;
         if(_prop.height == undefined) _prop.height = this.canvas.offsetHeight;
     
+        // 캔버스 크기 세팅
         this.Width = _prop.width; // 캔버스 너비
         this.Height = _prop.height; // 캔버스 높이
 
-        this.SettingNameWidth = 100; // 세팅 이름이 들어갈 영역 크기
+        // 캔버스 내부 옵션
+        this.LabelWidth = 100; // 세팅 이름이 들어갈 영역 크기
+        this.SpeedLabelHeight = 20; // 속도 라벨이 들어갈 가로 하단 영역 크기
+
     }
-    
 
     // 캔버스 너비 설정
     set Width(_width){
@@ -37,12 +40,20 @@ class SpeedChart{
 
     // 데이터 세팅 및 검증
     setData(_data){
+        console.log(_data);
 
-    }
+        // 데이터를 검증한다
+        try {
+            
+        } catch (error) {
+            
+        }
 
-    // 데이터의 크랭크 수를 비교해 Y 축 그리드를 구함
-    setAxisY(){
-        
+        // 검증 완료 후 스피드 차트 세팅
+        this.SpeedChart = _data;
+
+        // 최대 속도, 최소 속도 데이터가 있다면 세팅. 데이터와 무관하게 다시 세팅할 수 있다.
+        this.setAxisX(this.SpeedChart.min_speed,this.SpeedChart.max_speed);
     }
 
     // 최소 속도, 최대 속도를 받아 X 축 그리드 기준을 구함
@@ -55,46 +66,72 @@ class SpeedChart{
         // 5km씩 여유를 두도록 세팅
         this.MinSpeed = Math.ceil(_min)-5;
         this.MaxSpeed = Math.round(_max)+5;
-
-        // 캔버스의 차트 영역에서 1km/h 당 픽셀 크기를 계산
-        this.CanvasPixcel = (this.canvas.width - this.SettingNameWidth) / ( this.MaxSpeed - this.MinSpeed );
-    
-        console.log(this.MinSpeed,this.MaxSpeed,this.CanvasPixcel);
-
-        // 계산된 축 크기로 그리드를 그림
-        this.setRuller();
+        console.log("Min Speed =>",this.MinSpeed,'\n','Max Speed =>',this.MaxSpeed);
     }
 
-    // KM 그리드 그리기
-    setRuller(){
-        let ScaleSize;
+    // 캔버스의 차트 영역에서 주어진 km의 픽셀 위치를 계산
+    getXbySpeed(_speed){
 
-        // 기존 그리드 초기화
-        let ruller = this.canvas.getContext('2d');    
-        ruller.clearRect(0,0,this.canvas.width,this.canvas.height);
+        // 최소, 최대축 세팅이 되어 있지 않다면 에러 처리
+        if(this.MinSpeed == undefined || this.MaxSpeed == undefined) throw new Error("Set Minimun Speed and Maximun Speed first!");
 
-        // 최소 스피드 1부터 반복하며 그리드를 그림
+        // 라벨이 들어갈 위치를 제외한 후, 1km당 픽셀 위치를 계산
+        let SpeedPixcel = (this.canvas.width - this.LabelWidth) / ( this.MaxSpeed - this.MinSpeed );
+
+        // X 좌표 확정 후 출력
+        return SpeedPixcel * _speed;
+    }
+
+    // 크랭크 순서에 따라 차트에서의 Y값 위치를 가져옴
+    getYbyCrankIndex(_index){
+
+        // 0부터 시작하므로, +1
+        let i = _index + 1;
+
+        // 데이터가 세팅 되어 있지 않다면, 에러 처리
+        if(this.SpeedChart == undefined) throw new Error("Set Speed Data first!!");
+
+        // 크랭크 수를 가져온 후, Y축의 중간 위치를 반환함
+        let length = this.SpeedChart.crank.length
+        let Y = (this.canvas.height - this.SpeedLabelHeight) / length;
+        let Y2 = Y / 2
+
+        return (Y*i) - Y2;
+    };
+
+    /**
+     * X 축 속도 그리드 그리기
+     */
+    drawRuller(){
+
+        // 최소, 최대축 세팅이 되어 있지 않다면 에러 처리
+        if(this.MinSpeed == undefined || this.MaxSpeed == undefined) throw new Error("Set Minimun Speed and Maximun Speed first!");
+
+        // 최소 스피드부터 최대 스피드까지 반복하며 그리드를 그림
+        let ruller = this.canvas.getContext('2d');
+        let text = this.canvas.getContext('2d');
         for (let i = this.MinSpeed; i < this.MaxSpeed; i++) {
-            let pointer = i - this.MinSpeed;
+            // 픽셀 위치를 가져옴
+            let x = this.getXbySpeed(i);
     
-            // 10단위로 검은색 강조
+            // 10단위로 검은색 강조, 텍스트 쓰기
             let rullerColor = '#e6e6e6';
-            if(i%10 == 0) rullerColor = '#666666';
-
-            // X 좌표 확정
-            let x = this.SettingNameWidth + this.CanvasPixcel * pointer;
+            if(i%10 == 0) {
+                rullerColor = '#666666';
+                text.font = (this.SpeedLabelHeight/1.4)+'px sanserif';
+                text.strokeStyle = 'black';
+                text.textAlign = 'center'
+                text.strokeText(i + "km/h",x,this.canvas.height - (this.SpeedLabelHeight / 2) );
+            } 
 
             // 그리드 그리기
             ruller.beginPath();
             ruller.moveTo(x,0);
-            ruller.lineTo(x,this.canvas.height);
+            ruller.lineTo(x,this.canvas.height - this.SpeedLabelHeight);
             ruller.lineWidth = 1;
             ruller.strokeStyle = rullerColor;
             ruller.stroke();
-        
         }
-    
-        return ruller;
     }
     
     drawBar(_min,_max,_color,_text){
@@ -122,6 +159,33 @@ class SpeedChart{
         ctx.fillText(_text,start + (width/2), this.barHeight);
     
         return ctx;
+    }
+
+    /**
+     * 최종 드로우
+     */
+    drawAll(){
+
+        // 기존 드로우 초기화
+        let clear = this.canvas.getContext('2d');
+        clear.clearRect(0,0,this.canvas.width,this.canvas.height);
+
+        // 드로우 시작
+        this.drawRuller();
+
+        // 테스트 Y 그리드 그리기
+        let ruller = this.canvas.getContext('2d');
+        this.SpeedChart.crank.forEach((element,index)=> {
+            let y = this.getYbyCrankIndex(index);
+
+            ruller.beginPath();
+            ruller.moveTo(0,y);
+            ruller.lineTo(this.canvas.width,y);
+            ruller.lineWidth = 1;
+            ruller.strokeStyle = "red";
+            ruller.stroke();    
+        });
+
     }
     
 }
