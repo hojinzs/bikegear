5afa2adbfc479de9cded89e2955399454f1e7d1c
Steve Lee <hojinzs@gmail.com>
2020-04-08 22:44:43 +0900

    - 장소 추천글 등록 / 삭제 API 연결 (UI 수정 필요)

--- src/components/apps_infra_place/place-recommend-list.vue
+++ src/components/apps_infra_place/place-recommend-list.vue
@@ -2,54 +2,8 @@
     <div class="recommend-comment-section">
         <!-- 내가 작성한 추천 글 -->
         <div class="my-comment">
-            
-            <transition name="tab-fade" mode="out-in">
 
-                <!-- 인증되지 않았을 경우 -->
-                <div v-if="false" class="section" key="loginRequire">
-                    로그인이 필요합니다.
-                </div>
-
-                <!-- 인증됨, 사용자 인증됨 -->
-                <div class="section" key="ready"
-                v-if="( user_data.login == true && post_comment.status == 'hidden')">
-                    <div class="lumi-button-full">
-                        <button class="lumi-button lumi-button-black"
-                            @click="toggle_showNewCommentForm(true)">추천글 쓰기</button>
-                    </div>
-                </div>
-
-                <!-- 인증됨, 등록 클릭 -->
-                <div class="section" key="posting"
-                v-if="( user_data.login == true && post_comment.status == 'posting')">
-                    <form class="lumi-box lumi-box-block-grey" action="submit" @submit.prevent="postRecommentPost">
-                        <label for="post_recomment_comment">새로운 추천글을 등록합니다.</label>
-                        <div class="lumi-text-area-wrapper">
-                            <vue-extended-textarea class="lumi-input-liner" id="post_recomment_comment"
-                                v-model="post_comment.text" />
-                        </div>
-                        <div class="lumi-button-group">
-                            <button class="lumi-button lumi-button-flat-dark"
-                                @click.prevent="toggle_showNewCommentForm(false)">취소</button>
-                            <button type="submit" class="lumi-button lumi-button-black"
-                                :disabled="(this.post_comment.ajax_status != 'ready')">작성</button>
-                            </div>
-                        <div>
-                            <span v-show="post_comment.ajax_fail_message" class="warning"
-                            >{{ post_comment.ajax_fail_message }}</span>
-                        </div>
-                    </form>
-                </div>
-
-                <!-- 인증됨, 내가 작성한 추천글 -->
-                <div class="section" key="done"
-                v-if="( user_data.login == true && post_comment.status == 'posted' && post_comment.user_posted != null)">
-                    <b>내가 작성한 추천글</b>
-                    <recommend-comment :recommend="post_comment.user_posted" />
-                    <a @click="toggle_showNewCommentForm(true)">업데이트 하기</a>
-                </div>
-            
-            </transition>
+            <place-recommend-write :place="place"/>
 
             <div class="section">
                 <hr>
@@ -86,8 +40,9 @@
 import axios from 'axios'
 // import { recommend_comment } from '@/plugins/sampledb'
 
+import placeRecommendWrite from './place-recommend-write'
+
 import recommendComment from './place-recommend-comment'
-import vueExtendedTextarea from '@/components/interface/vue-extended-textarea'
 
 export default {
     name: 'place-recommend-list',
@@ -99,7 +54,7 @@ export default {
     },
     components: {
         'recommend-comment': recommendComment,
-        'vue-extended-textarea' : vueExtendedTextarea
+        'place-recommend-write' : placeRecommendWrite,
     },
     data(){
         let ajax_address = '//'+process.env.VUE_APP_API_HOST+'/v1/places/'
@@ -107,16 +62,6 @@ export default {
             +'/recommends'
 
         return {
-            user_data: {
-                login: true,
-            },
-            post_comment: {
-                status: 'hidden', // [hidden, posting, posted]
-                text: '',
-                ajax_status: 'ready', // [ready, posting, success, fail]
-                ajax_fail_message: null,
-                user_posted: undefined
-            },
             recommend_comment: {
                 list: [],
                 ajax_address: ajax_address,
@@ -125,66 +70,7 @@ export default {
             },
         }
     },
-    computed: {
-        post_button_disabled(){
-            console.log("disabled => ", this.post_comment.ajax_status != 'ready')
-            return this.post_comment.ajax_status != 'ready'
-        }
-    },
     methods:{
-        toggle_showNewCommentForm(show){
-            if(show){
-                this.post_comment.status = 'posting'
-            } else {
-                if(this.user_posted){
-                    this.post_comment.status = 'posted'
-                } else {
-                    this.post_comment.status = 'hidden'
-                }
-                this.clearTextArea()
-            }
-        },
-        clearTextArea(){
-            this.post_comment.text = ''
-            this.post_comment.ajax_fail_message = ''
-        },
-        postRecommentPost(){
-            // Set Dummy Data
-            let newPost = {
-                "comment" : this.post_comment.text,
-                "author" : "Moderator",
-                "written_at" : new Date()
-            }
-            let form = JSON.stringify(newPost)
-
-            //Before
-            this.post_comment.ajax_status = 'posting'
-            console.log("set form => ", form)
-
-            //Post
-            let ajax_success = Math.round(Math.random())
-            setTimeout(()=>{
-                let res = JSON.parse(form)
-
-                if(ajax_success){
-                    console.log(res)
-                    this.post_comment.status = 'posted'
-                    this.post_comment.ajax_status = 'success'
-                    this.post_comment.user_posted = res
-                    this.clearTextArea()
-                } else {
-                    this.post_comment.ajax_status = 'fail'
-                    this.post_comment.ajax_fail_message = '제대로 등록되지 않았습니다.'
-                }
-
-                //Complete
-                console.log("success => ",ajax_success)
-                setTimeout(() => {
-                    this.post_comment.ajax_status = 'ready'
-
-                },1000)
-            },1200)
-        },
         getRecommendCommentList(){
 
             //get data
@@ -193,7 +79,6 @@ export default {
                 url: this.recommend_comment.ajax_address
             })
             .then(res => {
-                console.log("get Recommend Data => ",res)
 
                 // 코멘트 배열에 데이터 넣기
                 let comments = res.data.data
---
+++ src/components/apps_infra_place/place-recommend-write.vue
@@ -0,0 +1,211 @@
+<template>
+    <transition name="tab-fade" mode="out-in">
+
+        <!-- 인증되지 않았을 경우 -->
+        <div v-if="userData == null"
+        class="section" key="loginRequire">
+
+            로그인이 필요합니다.
+
+        </div>
+
+        <!-- 인증됨, 로딩중 -->
+        <div v-if="( userData && post_comment.status == 'loading' )"
+        class="section" key="loading">
+
+            사용자의 글을 가져오고 있습니다.
+
+        </div>
+
+        <!-- 인증됨, 새로운 글을 등록하거나, 등록된 글 없음 -->
+        <div v-if="( userData && post_comment.user_posted == null && post_comment.status == 'ready' )"
+        class="section" key="ready">
+
+            <div class="lumi-button-full">
+                <button class="lumi-button lumi-button-black"
+                    @click="toggle_showNewCommentForm(true)">추천글 쓰기</button>
+            </div>
+
+        </div>
+
+        <!-- 인증됨, 등록 클릭 -->
+        <div v-if="( userData && post_comment.status == 'posting' )"
+        class="section" key="posting">
+
+            <form class="lumi-box lumi-box-block-grey" action="submit" @submit.prevent="postRecommentPost">
+                <label for="post_recomment_comment">새로운 추천글을 등록합니다.</label>
+                <div class="lumi-text-area-wrapper">
+                    <vue-extended-textarea class="lumi-input-liner" id="post_recomment_comment"
+                        v-model="post_comment.text" />
+                </div>
+                <div class="lumi-button-group">
+                    <button class="lumi-button lumi-button-flat-dark"
+                        @click.prevent="toggle_showNewCommentForm(false)">취소</button>
+                    <button type="submit" class="lumi-button lumi-button-black"
+                        :disabled="(this.post_comment.ajax_status != 'ready')">작성</button>
+                    </div>
+                <div>
+                    <span v-show="post_comment.ajax_fail_message" class="warning"
+                    >{{ post_comment.ajax_fail_message }}</span>
+                </div>
+            </form>
+        </div>
+
+        <!-- 인증됨, 내가 작성한 추천글 -->
+        <div v-if="( userData && post_comment.user_posted )"
+        class="section" key="done">
+
+            <b>내가 작성한 추천글</b>
+            <recommend-comment :recommend="post_comment.user_posted" />
+            <div>
+                <div>
+                    <a @click="toggle_showNewCommentForm(true)">업데이트 하기</a>
+                </div>
+                <div>
+                    <a @click="destroyRecommentPost()">삭제하기</a>
+                </div>
+            </div>
+
+        </div>
+    
+    </transition>
+</template>
+
+<script>
+import recommendComment from './place-recommend-comment'
+import vueExtendedTextarea from '@/components/interface/vue-extended-textarea'
+import axios from 'axios'
+
+export default {
+    name: 'place-recommend-write',
+    components:
+    {
+        'recommend-comment': recommendComment,
+        'vue-extended-textarea' : vueExtendedTextarea,
+    },
+    props:
+    {
+        place: {
+            type: Object,
+            required: true,
+        }
+    },
+    data()
+    {
+        let post_comment_ajax_address = '//'+process.env.VUE_APP_API_HOST+'/v1/places/'
+            +this.place.id
+            +'/recommends';
+
+        let user_comment_ajax_address = '//'+process.env.VUE_APP_API_HOST+'/v1/user/places/'
+            +this.place.id
+            +'/recommends';
+
+        return {
+            user_comment_ajax_address,
+            post_comment: {
+                status: 'ready', // [ready, posting]
+                text: '',
+                ajax_address: post_comment_ajax_address,
+                ajax_status: 'ready', // [ready, posting, success, fail]
+                ajax_fail_message: null,
+                user_posted: null
+            },
+        }
+
+    },
+    computed:
+    {
+        userData() {
+            return this.$store.state.user.user_data
+        }
+    },
+    methods: 
+    {
+        toggle_showNewCommentForm(show){
+            if(show){
+                this.post_comment.status = 'posting'
+            } else {
+                this.post_comment.status = 'ready'
+                this.clearTextArea()
+            }
+        },
+        clearTextArea(){
+            this.post_comment.text = ''
+            this.post_comment.ajax_fail_message = ''
+        },
+        getUserPost(){
+            this.post_comment.status = 'loading'
+
+            axios.get(this.user_comment_ajax_address)
+                .then(res => {
+                    let userPost = res.data.data.find(item => item.hidden == false)
+                    
+                    if(userPost){
+                        this.post_comment.user_posted = userPost
+                    }
+                })
+                .finally(() => {
+                    this.post_comment.status = 'ready'
+                })
+        },
+        postRecommentPost(){
+            // Set Dummy Data
+            let newPost = {
+                "comment" : this.post_comment.text,
+                "author" : "Moderator",
+                "written_at" : new Date()
+            }
+            let form = JSON.stringify(newPost)
+
+            //Before
+            this.post_comment.ajax_status = 'posting'
+            console.log("set form => ", form)
+
+            axios.post(this.post_comment.ajax_address,{
+                'content' : this.post_comment.text
+            })
+                .then(res => {
+                    this.post_comment.status = 'posted'
+                    this.post_comment.ajax_status = 'success'
+                    this.post_comment.user_posted = res.data.data
+                    this.clearTextArea()
+                })
+                .catch(() => {
+                    this.post_comment.ajax_status = 'fail'
+                    this.post_comment.ajax_fail_message = '제대로 등록되지 않았습니다.'
+                })
+                .finally(() => {
+                    setTimeout(() => this.post_comment.ajax_status = 'ready' ,1000)
+                })
+        },
+        destroyRecommentPost(){
+
+            let ok = confirm("작성한 추천글을 삭제합니다. \n계속하시겠습니까?")
+            if(ok){
+                this.post_comment.ajax_status = 'posting'
+                axios.delete(this.post_comment.ajax_address+'/'+this.post_comment.user_posted.id)
+                    .then(() => {
+                        this.post_comment.ajax_status = 'success'
+                        this.post_comment.user_posted = null
+                    })
+                    .catch(() => {
+                        this.post_comment.ajax_status = 'fail'
+                    })
+                    .finally(() => {
+                        setTimeout(() => this.post_comment.ajax_status = 'ready' ,1000)
+                    })
+            }
+            
+
+        }
+    },
+    mounted()
+    {
+        this.getUserPost()
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
