53a575a8287e32e1cb2a88e42be1d9c27b0a64bb
Steve Lee <hojinzs@gmail.com>
2020-05-01 04:41:42 +0900

    - vue-naver-map 버전업 - naver-map 직접 접근 연결 추가 - 레이아웃 변경 준비

--- src/components/apps_infra_map/index.vue
+++ src/components/apps_infra_map/index.vue
@@ -1,133 +1,159 @@
 <template>
     <div id="app">
-
-<!--        <div>-->
-<!--            <div id="mapLeftSection">-->
-
-<!--            </div>-->
-<!--            <div id="mapRightSection">-->
-<!--                <div id="mapOverMenu">-->
-
-<!--                </div>-->
-<!--                <div id="Map">-->
-
-<!--                </div>-->
-
-<!--            </div>-->
-<!--        </div>-->
-
-        <div id="MenuTop">
-            <div class="lumi-flex-slider-wrapper scroll-free">
-                <ul class="lumi-flex-slider" style="padding-left: 1em">
-
-                    <!-- Loading -->
-                    <li class="lumi-flex-slider-item"
-                        v-if="infraList_status === 'loading'">
-                        <button class="infra-indicator lumi-button lumi-button-border-round lumi-button-block-white lumi-button-shadow">
-                            <img class="loading" src="/images/Spinner-1s-104px.gif">
-                            <!-- :style="{color: item.color}"/> -->
-                            Loading
-                        </button>
-                    </li>
-
-                    <!-- Load Error -->
-                    <li class="lumi-flex-slider-item"
-                        v-if="infraList_status === 'error'">
-                        <button class="infra-indicator lumi-button lumi-button-border-round lumi-button-block-white lumi-button-shadow"
-                                :style="{color: 'red'}">
-                            <font-awesome-icon class="infra-icon"
-                                               :icon="'exclamation-triangle'" />
-                            Error
-                        </button>
-                    </li>
-
-                    <!-- LOOP START -->
-                    <li class="lumi-flex-slider-item"
-                        v-for="(item,index) in FeaturedItems"
-                        :key="index">
-                        <button class="infra-indicator lumi-button lumi-button-border-round lumi-button-block-white lumi-button-shadow"
-                                @click="toggleFilter(item.name)">
-                            <span class="dot infra-indicator-item"
-                                  :class="{'active' : (filter.tags.indexOf(item.name) !== -1) }">
-                            </span>
-                            <font-awesome-icon class="infra-icon infra-indicator-item"
-                                               :icon="item.icon"
-                                               :style="{color: item.color}"/>
-                            <span class="infra-indicator-item">
-                                {{item.label}}
-                            </span>
-                            <span class="infra-count-int infra-indicator-item"
-                                  :style="{backgroundColor: item.color}">
-                                {{item.count}}
-                            </span>
-                        </button>
-                    </li>
-                    <!-- LOOP END -->
-
-                </ul>
-            </div>
-        </div>
-
-        <div id="MenuLeft">
-        </div>
-
-        <div id="MenuBottom">
-            <lumiCaroucel
-                    :speedStiky="700"
-                    :positionStiky="'center'"
-                    :async="true"
-                    @loaded="setCaroucel"
-                    @focused="getFocused">
-
-                <lumiCaroucelSlide
-                        v-for="(place,index) in DisplayItems"
-                        :key="index"
-                        @onClick="getFocused(index)">
-
-                    <PlaceCard
-                            :title="place.name"
-                            :thumbnail_img_url="place.Image"
-                            :extention_toggled="(DisplayItems_toggled === index)">
-                        {{place.type}}
-                        <template v-slot:expention>
-                            <button class="lumi-button-liner"
-                                    @click.stop="showDetail(place.id)">
-                                정보 보기
-                            </button>
-                        </template>
-                    </PlaceCard>
-
-                </lumiCaroucelSlide>
-
-            </lumiCaroucel>
-        </div>
-
-        <naver-maps
-            class="maps"
-            style="width: 100%; height: 100%;"
-            :width="100"
-            :height="100"
-            :mapOptions="mapOptions"
-            @load="onLoad"
+        <div
+            id="mapLeftSection"
+            :class="{
+            'hidden': !showLeftMenu
+            }"
         >
+            asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf
+        </div>
+        <div id="mapRightSection" ref="rightSection">
 
-            <naver-circle
-                ref="currentPosition"
-                v-if="currentPosition.status === 'display'"
-                :lat="currentPosition.latitude"
-                :lng="currentPosition.longitude"
-                :radius="100"
-                fillColor="#ff0000"
-            />
+            <div id="MenuTop">
+                <div class="lumi-flex-slider-wrapper scroll-free">
+                    <ul class="lumi-flex-slider" style="padding-left: 1em">
 
-            <naver-marker
-                v-for="(item, index) in DisplayItems"
-                :key="index" :lat="item.geoPoint.latitude"
-                :lng="item.geoPoint.longitude"
-                @click="getFocused(index)"
-            />
+                        <li class="lumi-flex-slider-item">
+                            <button
+                                    class="infra-indicator lumi-button lumi-button-border-round lumi-button-block-white lumi-button-shadow"
+                                    @click="toggleLeftMenu()"
+                            >
+                                A
+                            </button>
+                        </li>
 
-        </naver-maps>
+                        <!-- Loading -->
+                        <li class="lumi-flex-slider-item"
+                            v-if="infraList_status === 'loading'">
+                            <button class="infra-indicator lumi-button lumi-button-border-round lumi-button-block-white lumi-button-shadow">
+                                <img class="loading" src="/images/Spinner-1s-104px.gif">
+                                <!-- :style="{color: item.color}"/> -->
+                                Loading
+                            </button>
+                        </li>
+
+                        <!-- Load Error -->
+                        <li class="lumi-flex-slider-item"
+                            v-if="infraList_status === 'error'">
+                            <button class="infra-indicator lumi-button lumi-button-border-round lumi-button-block-white lumi-button-shadow"
+                                    :style="{color: 'red'}">
+                                <font-awesome-icon class="infra-icon"
+                                                   :icon="'exclamation-triangle'" />
+                                Error
+                            </button>
+                        </li>
+
+                        <!-- LOOP START -->
+                        <li class="lumi-flex-slider-item"
+                            v-for="(item,index) in FeaturedItems"
+                            :key="index">
+                            <button class="infra-indicator lumi-button lumi-button-border-round lumi-button-block-white lumi-button-shadow"
+                                    @click="toggleFilter(item.name)">
+                        <span class="dot infra-indicator-item"
+                              :class="{'active' : (filter.tags.indexOf(item.name) !== -1) }">
+                        </span>
+                                <font-awesome-icon class="infra-icon infra-indicator-item"
+                                                   :icon="item.icon"
+                                                   :style="{color: item.color}"/>
+                                <span class="infra-indicator-item">
+                            {{item.label}}
+                        </span>
+                                <span class="infra-count-int infra-indicator-item"
+                                      :style="{backgroundColor: item.color}">
+                            {{item.count}}
+                        </span>
+                            </button>
+                        </li>
+                        <!-- LOOP END -->
+
+                    </ul>
+                </div>
+                <div>
+                    <div class="lumi-flex-slider-wrapper scroll-free">
+                        <ul class="lumi-flex-slider" style="padding-left: 1em">
+                            <li class="lumi-flex-slider-item">
+                                <button
+                                        class="infra-indicator lumi-button lumi-button-border-round lumi-button-block-white lumi-button-shadow"
+                                        @click="getCurrentPosition()"
+                                >
+                                    A
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div id="MenuBottom">
+                <lumiCaroucel
+                        :speedStiky="700"
+                        :positionStiky="'center'"
+                        :async="true"
+                        @loaded="setCaroucel"
+                        @focused="getFocused">
+
+                    <lumiCaroucelSlide
+                            v-for="(place,index) in DisplayItems"
+                            :key="index"
+                            @onClick="getFocused(index)">
+
+                        <PlaceCard
+                                :title="place.name"
+                                :thumbnail_img_url="place.Image"
+                                :extention_toggled="(DisplayItems_toggled === index)">
+                            {{place.type}}
+                            <template v-slot:expention>
+                                <button class="lumi-button-liner"
+                                        @click.stop="showDetail(place.id)">
+                                    정보 보기
+                                </button>
+                            </template>
+                        </PlaceCard>
+
+                    </lumiCaroucelSlide>
+
+                </lumiCaroucel>
+            </div>
+
+            <div id="Map">
+
+                <naver-maps
+                    ref="Maps"
+                    class="maps"
+                    style="width: 100%; height: 100%;"
+                    :width="100"
+                    :height="100"
+                    :mapOptions="mapOptions"
+                    :initLayers="initLayers"
+                    @load="onLoad"
+                    @dragend="eventDragEnd"
+                    @click="eventDragEnd"
+                >
+
+                    <naver-marker
+                        ref="currentPosition"
+                        v-if="currentPosition.status === 'display'"
+                        :lat="currentPosition.latitude"
+                        :lng="currentPosition.longitude"
+                        :radius="100"
+                        fillColor="#ff0000"
+                    />
+
+                    <naver-marker
+                        v-for="(item, index) in DisplayItems"
+                        :key="index" :lat="item.geoPoint.latitude"
+                        :lng="item.geoPoint.longitude"
+                        @click="getFocused(index)"
+                        @dragend="eventDragEnd()"
+                    />
+
+                </naver-maps>
+
+            </div>
+
+        </div>
+
     </div>
 </template>
 
@@ -143,14 +169,15 @@ import axios from 'axios'
 import { featured, tags } from '../../plugins/sampledb'
 
 import { lumiCaroucel, lumiCaroucelSlide } from 'vue-luminus-style'
+import StyleVariable from '@/assets/variable.styl'
 import PlaceCard from './place_card'
 
-import geo from './geo_calc'
 import Tag from '../../plugins/journey66_tag'
 
 /**
  * vue-naver-maps
  * doc: https://shin-jaeheon.github.io/vue-naver-maps/#/
+ * doc: https://navermaps.github.io/maps.js.ncp/docs/naver.maps.html
  */
 Vue.use(naver,{
     clientID: process.env.VUE_APP_NAVER_API_CLIENT_KEY,
@@ -170,8 +197,10 @@ export default {
         let place_load_url = '//'+process.env.VUE_APP_API_HOST+'/v1/places'
         
         return {
+            // Map UI Control
+            showLeftMenu: true,
+            StyleVariable,
             // 샘플 데이터
-            geo,
             featured,
             // 샘플 데이터 edn
             tags,
@@ -189,11 +218,13 @@ export default {
             slide: null,
             // 지도 오브젝트
             map: null,
+            naverMap: null,
             mapOptions: {
                 lat: 37.4876,
                 lng: 127.1246,
                 zoom: 11,
             },
+            initLayers: ['BACKGROUND', 'BACKGROUND_DETAIL', 'POI_KOREAN', 'TRANSIT'],
             currentPosition: {
                 status: 'hidden', // ['hidden','loading','display']
                 latitude: null,
@@ -202,6 +233,9 @@ export default {
         }
     },
     computed: {
+        isMobile(){
+            return window.screen.width < Number(this.StyleVariable.containerWidth.replace("px",""))
+        },
         /**
          * 상단에 띄울 태그 목록을 제어하는 Getter
          */
@@ -239,7 +273,29 @@ export default {
          */
         onLoad(_map){
             this.map = _map
+            this.naverMap = window.naver.maps
+            
             this.getCurrentPosition()
+
+            this.naverMap.Event.addListener(_map.map,'dragend', () => {
+                console.log("DragEnd")
+            })
+        },
+        eventDragEnd(point){
+            alert("Asdf");
+            console.log("DragEnd =>", point)
+        },
+        toggleLeftMenu(show = !this.showLeftMenu){
+            this.showLeftMenu = show;
+
+            setTimeout(() => {
+                this.map.setSize({
+                    width: this.$refs['rightSection'].clientWidth,
+                    height: this.$refs['rightSection'].clientHeight
+                })
+
+                // this.map.refresh()
+            },100)
         },
         getCurrentPosition(){
             this.currentPosition.status = 'loading'
@@ -346,6 +402,9 @@ export default {
         },
     },
     mounted(){
+        if(this.isMobile){
+            this.toggleLeftMenu(false)
+        }
         this.getInfraData()
     },
     watch: {
@@ -367,28 +426,61 @@ export default {
     height 100%
     width 100%
     overflow hidden
-    #MenuLeft
-        position absolute
-        top 3rem
-        left 0
-        z-index 200
-    #MenuTop
-        position absolute
-        max-width 100%
-        top 3rem
-        z-index 200
-    #MenuBottom
-        position absolute
-        width 100%
-        bottom 0
-        z-index 200
-    #place_info
-        position absolute
-        top 4rem
-        left 50%
+    #mapLeftSection
+        padding-top 4rem
+        overflow-x hidden
+        height 100%
+    #mapRightSection
+        position relative
+        height 100%
+        #MenuTop
+            position absolute
+            max-width 100%
+            top 4rem
+            z-index 150
+        #MenuBottom
+            position absolute
+            width 100%
+            bottom 0
+            z-index 150
+        #Map
+            position absolute
+            width 100%
+            height 100%
+            z-index 100
+
+@media (min-width: $container_width)
+    #app
+        display flex
+        #mapLeftSection
+            flex none
+            width 30%
+            min-width 200px
+            max-width: 350px
+            &.hidden
+                width 0%
+                min-width 0px
+        #mapRightSection
+            flex 1 1 auto
+            overflow hidden
+
+@media (max-width: $container_width)
+    #app
+        #mapLeftSection
+            position absolute
+            background-color white
+            transform translateX(0%)
+            z-index 180
+            &.hidden
+                transform translateX(-100%)
 
 .activate
     border none
+
+.lumi-flex-slider-wrapper
+    ul
+        margin-top 0.5em
+        margin-bottom 0.5em
 
 .infra-indicator
     color $light_black
